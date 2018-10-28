31d73e09e96530ecbfeb4eddc7befce7c06e31cf
Paula McKinley <pmknyc@optimum.net>
2018-10-28 02:38:02 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_supplies.each do |season, holidays|
     puts "#{season.capitalize}:"
       holidays.each do |holiday, supplies|
-        puts "  #{holiday}:  #{supplies.join.capitalize}"
+        puts "  #{holiday.}:  #{supplies.join.capitalize}"
     end
   end
 end
